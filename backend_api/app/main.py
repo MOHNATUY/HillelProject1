cd296e9eb9df5adf3ebd6c9cd33db76433f9ba7e
MitzukiM <johndoe@example.com>
2025-06-30 18:58:48 +0300

    setup some backend files

--- backend_api/app/main.py
+++ backend_api/app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+#
+#
+# app = FastAPI(root_path="/api", root_path_in_servers=True)
+#
+#
+# @app.get("/")
+# async def index():
+#     return {"status2332": 200}
+from app_factory import get_application
 
-
-app = FastAPI(root_path="/api", root_path_in_servers=True)
-
-
-@app.get("/")
-async def index():
-    return {"status2332": 200}
+app = get_application()
